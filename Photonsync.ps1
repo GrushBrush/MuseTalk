4199e03a5d5bd1af8b55691a89c2c4397b1c42dd
MRGN CTO JiiT <56372379+JiiT-Tech@users.noreply.github.com>
2025-10-30 15:25:24 +0700

    change to gst_15

--- Photonsync.ps1
+++ Photonsync.ps1
@@ -1,1 +1,2 @@
-python -m scripts.realtime_stream_sync --inference_config configs/inference/realtime.yaml  --skip_save_images
+python -m scripts.realtime_stream_gst_15 --inference_config configs/inference/realtime.yaml  --skip_save_images
+
